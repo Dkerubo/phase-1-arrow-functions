668b27bc306ac023fc74a22d3465789c2f38b528
root <root@Kerry>
2024-09-23 00:07:46 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,17 @@
+// 1) has a function expression called divide
+const divide = function() {
+    // 2) divide divides 2000 by 100
+    return 2000 / 100;
+};
+
+// 3) has an arrow function called square
+const square = (x) => {
+    // 4) square arrow function takes one parameter and multiplies it times itself
+    return x * x;
+};
+
+// 5) has an arrow function called add
+const add = (a, b) => {
+ // 6) add arrow function takes two parameters and adds them together
+    return a + b;
+};
